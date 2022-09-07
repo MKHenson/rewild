7fefb4bdeb57b45d57b502ba8b174111747ba3a4
Mathew <mat@salteese.com>
2022-09-07 22:10:58 +0100

    Added upload & list functions to server

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,48 @@
 import * as express from "express";
-
+import { Storage } from "@google-cloud/storage";
+import * as Multer from "multer";
 const app = express();
 const port = 4000;
 
+const projectId = "";
+const keyFilename = "";
+const bucketName = "";
+
+const storage = new Storage({ projectId, keyFilename });
+const bucket = storage.bucket(bucketName);
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fieldSize: 5 * 1024 * 1024, // 5 megs
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("ok");
+});
+
+app.get("/files", async (req, res) => {
+  const [files] = await bucket.getFiles();
+  const toRet = files.map((f) => ({
+    url: f.publicUrl(),
+    meta: f.metadata,
+  }));
+  res.send(toRet);
+});
+
+app.post("/upload", multer.single("file"), (req, res) => {
+  try {
+    if (req.file) {
+      const blob = bucket.file(req.file.originalname);
+      const blobStream = blob.createWriteStream();
+      blobStream.on("finish", () => {
+        res.status(200).send("success");
+      });
+      blobStream.end(req.file.buffer);
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
 });
 
 app.listen(port, () => {});
