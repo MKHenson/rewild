bd805d30ebae24373296dd135aa180e7ab0c9c33
MKHenson <mat@webinate.net>
2022-10-11 23:27:00 +0100

    Fixed issues and routed project with editor component

---
+++ src/ts/ui/application/editor/EditorSplash.tsx
@@ -0,0 +1,47 @@
+import { Component, onMount } from "solid-js";
+import { styled } from "solid-styled-components";
+import { Routes, Route, useNavigate } from "@solidjs/router";
+import { Editor } from "./Editor";
+import { GameManager } from "../../../core/GameManager";
+import { UIEventManager } from "../../../core/UIEventManager";
+import { ApplicationEventType } from "../../../../common/EventTypes";
+import { ProjectSelector } from "./ProjectSelector";
+
+interface Props {
+  gameManager: GameManager;
+  eventManager: UIEventManager;
+  onQuit: () => void;
+}
+
+export const EditorSplash: Component<Props> = (props) => {
+  const navigate = useNavigate();
+  const onHomeClick = () => {
+    props.onQuit();
+  };
+
+  onMount(() => {
+    props.eventManager.triggerUIEvent(ApplicationEventType.StartEditor);
+  });
+
+  return (
+    <StyledContainer>
+      <Routes>
+        <Route
+          path="/"
+          element={<ProjectSelector onBack={onHomeClick} open onOpen={(uid) => navigate(`/editor/${uid}`)} />}
+        />
+        <Route path="/:project" element={<Editor onHome={onHomeClick} />} />
+      </Routes>
+    </StyledContainer>
+  );
+};
+
+const StyledContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  position: absolute;
+  display: flex;
+  box-sizing: border-box;
+`;
