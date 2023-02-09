0f5a4c818f701e22093eddd371eac812a6e570e5
MKHenson <mat@webinate.net>
2023-02-09 16:18:09 +0000

    No react or solidjs

---
+++ src/ts/initJSX.tsx
@@ -0,0 +1,65 @@
+function jsx<T extends JSX.Tag = JSX.Tag>(
+  tag: T,
+  attributes: { [key: string]: any } | null,
+  ...children: Node[]
+): JSX.Element;
+function jsx(tag: JSX.Component, attributes: Parameters<typeof tag> | null, ...children: Node[]): Node;
+function jsx(tag: JSX.ComponentStatic, attributes: Parameters<typeof tag> | null, ...children: Node[]): Node;
+function jsx(
+  tag: JSX.Tag | JSX.Component | JSX.ComponentStatic,
+  attributes: { [key: string]: any } | null,
+  ...children: Node[]
+) {
+  // Check if this is a web component
+  if (typeof tag === "function" && (tag as JSX.ComponentStatic).tagName) {
+    const element = document.createElement((tag as JSX.ComponentStatic).tagName!) as JSX.Element;
+    element.props = attributes!;
+    appendChildren(element, children);
+    return element;
+  }
+
+  // Check if a functional component
+  if (typeof tag === "function") {
+    return tag(attributes ?? {}, children);
+  }
+  const element = document.createElement(tag);
+
+  // Assign attributes:
+  let map = attributes ?? {};
+  let prop: keyof typeof map;
+  for (prop of Object.keys(map) as any) {
+    // Extract values:
+    prop = prop.toString();
+    const value = map[prop] as any;
+    const anyReference = element as any;
+    if (typeof anyReference[prop] === "undefined") {
+      // As a fallback, attempt to set an attribute:
+      element.setAttribute(prop, value);
+    } else {
+      anyReference[prop] = value;
+    }
+  }
+
+  // append children
+  appendChildren(element, children);
+  return element;
+}
+
+function appendChildren(element: HTMLElement, children: Node[]) {
+  // append children
+  for (let child of children) {
+    if (typeof child === "string") {
+      element.innerText += child;
+      continue;
+    }
+    if (Array.isArray(child)) {
+      element.append(...child);
+      continue;
+    }
+
+    if (child) element.appendChild(child);
+  }
+}
+
+(window as any).jsx = jsx;
+(window as any).css = (val: string) => val;
