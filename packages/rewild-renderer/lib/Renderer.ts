038e4f097aa182d80c308f05b1b54f0d9e2453ed
MKHenson <mat@webinate.net>
2025-01-16 22:02:35 +0000

    Drawing multiple objects from an array

--- packages/rewild-renderer/lib/Renderer.ts
+++ packages/rewild-renderer/lib/Renderer.ts
@@ -6,35 +6,46 @@ struct OurVertexShaderOutput {
   @location(0) color: vec4f,
 };
 
+// Doesnt change much 
+struct OurStruct {
+  color: vec4f,
+  offset: vec2f,
+};
+
+// Does change much
+struct OtherStruct {
+  scale: vec2f,
+};
+
+@group(0) @binding(0) var<uniform> ourStruct: OurStruct;
+@group(0) @binding(1) var<uniform> otherStruct: OtherStruct;
+
 
 @vertex
 fn vs(
-  @builtin(vertex_index) VertexIndex : u32
-) -> OurVertexShaderOutput {
+  @builtin(vertex_index) vertexIndex : u32
+) -> @builtin(position) vec4f {
   var pos = array<vec2f, 3>(
     vec2f(0.0, 0.5),
     vec2f(-0.5, -0.5),
     vec2f(0.5, -0.5)
   );
 
-  var color = array<vec4f, 3>(
-    vec4f(1.0, 0.0, 0.0, 0.5), // red
-    vec4f(0.0, 1.0, 0.0, 0.5), // green
-    vec4f(0.0, 0.0, 1.0, 0.5)  // blue
-  );
-
-  var output: OurVertexShaderOutput;
-  output.position = vec4f(pos[VertexIndex], 0.0, 1.0);
-  output.color = color[VertexIndex];
-  return output;
+  return vec4f(
+    pos[vertexIndex] * otherStruct.scale + ourStruct.offset, 0.0, 1.0);
 }
   
 @fragment
-fn fs(fsInput: OurVertexShaderOutput) -> @location(0) vec4f {
-  return fsInput.color;
+fn fs() -> @location(0) vec4f {
+  return ourStruct.color;
 }`;
 
 const sampleCount = 4;
+
+// offsets to the various uniform values in float32 indices
+const kColorOffset = 0;
+const kOffsetOffset = 4;
+const kScaleOffset = 0;
 
 export class Renderer {
   device: GPUDevice;
@@ -50,6 +61,15 @@ export class Renderer {
   renderTarget: GPUTexture | undefined;
   presentationFormat: GPUTextureFormat;
   initialized: boolean;
+
+  kNumObjects = 100;
+  objectInfos: {
+    scale: f32;
+    uniformBuffer: GPUBuffer;
+    bindGroup: GPUBindGroup;
+    uniformValues: Float32Array;
+  }[] = [];
+
   private onFrameHandler: () => void;
 
   constructor() {
@@ -118,6 +138,69 @@ export class Renderer {
         count: sampleCount,
       },
     });
+
+    // A random number between [min and max)
+    // With 1 argument it will be [0 to min)
+    // With no arguments it will be [0 to 1)
+    const rand = (min?: number, max?: number) => {
+      if (min === undefined) {
+        min = 0;
+        max = 1;
+      } else if (max === undefined) {
+        max = min;
+        min = 0;
+      }
+      return min + Math.random() * (max - min);
+    };
+
+    // create 2 buffers for the uniform values
+    const staticUniformBufferSize =
+      4 * 4 + // color is 4 32bit floats (4bytes each)
+      2 * 4 + // offset is 2 32bit floats (4bytes each)
+      2 * 4; // padding
+    const uniformBufferSize = 2 * 4; // scale is 2 32bit floats (4bytes each)
+
+    for (let i = 0; i < this.kNumObjects; ++i) {
+      const staticUniformBuffer = device.createBuffer({
+        label: `static uniforms for obj: ${i}`,
+        size: staticUniformBufferSize,
+        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+      });
+
+      // These are only set once so set them now
+      {
+        const uniformValues = new Float32Array(staticUniformBufferSize / 4);
+        uniformValues.set([rand(), rand(), rand(), 1], kColorOffset); // set the color
+        uniformValues.set([rand(-0.9, 0.9), rand(-0.9, 0.9)], kOffsetOffset); // set the offset
+
+        // copy these values to the GPU
+        device.queue.writeBuffer(staticUniformBuffer, 0, uniformValues);
+      }
+
+      // create a typedarray to hold the values for the uniforms in JavaScript
+      const uniformValues = new Float32Array(uniformBufferSize / 4);
+      const uniformBuffer = device.createBuffer({
+        label: `changing uniforms for obj: ${i}`,
+        size: uniformBufferSize,
+        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+      });
+
+      const bindGroup = device.createBindGroup({
+        label: `bind group for obj: ${i}`,
+        layout: pipeline.getBindGroupLayout(0),
+        entries: [
+          { binding: 0, resource: { buffer: staticUniformBuffer } },
+          { binding: 1, resource: { buffer: uniformBuffer } },
+        ],
+      });
+
+      this.objectInfos.push({
+        scale: rand(0.2, 0.5),
+        uniformBuffer,
+        uniformValues,
+        bindGroup,
+      });
+    }
 
     requestAnimationFrame(this.onFrameHandler);
 
@@ -193,6 +276,18 @@ export class Renderer {
 
       const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
       passEncoder.setPipeline(this.pipeline);
+
+      // Set the uniform values in our JavaScript side Float32Array
+      const aspect = canvas.width / canvas.height;
+
+      for (const { scale, bindGroup, uniformBuffer, uniformValues } of this
+        .objectInfos) {
+        uniformValues.set([scale / aspect, scale], kScaleOffset); // set the scale
+        device.queue.writeBuffer(uniformBuffer, 0, uniformValues);
+        passEncoder.setBindGroup(0, bindGroup);
+        passEncoder.draw(3); // call our vertex shader 3 times
+      }
+
       passEncoder.draw(3);
       passEncoder.end();
 
