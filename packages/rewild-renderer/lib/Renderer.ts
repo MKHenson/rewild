6674e467d5e8189e1fc48d0868efd1dcc2e312a5
MKHenson <mat@webinate.net>
2025-01-18 17:41:30 +0000

    Added storage buffer support

--- packages/rewild-renderer/lib/Renderer.ts
+++ packages/rewild-renderer/lib/Renderer.ts
@@ -7,6 +7,77 @@ const sampleCount = 4;
 const kColorOffset = 0;
 const kOffsetOffset = 4;
 const kScaleOffset = 0;
+// create 2 storage buffers
+const staticUnitSize =
+  4 * 4 + // color is 4 32bit floats (4bytes each)
+  2 * 4 + // offset is 2 32bit floats (4bytes each)
+  2 * 4; // padding
+const changingUnitSize = 2 * 4; // scale is 2 32bit floats (4bytes each)
+
+// A random number between [min and max)
+// With 1 argument it will be [0 to min)
+// With no arguments it will be [0 to 1)
+const rand = (min?: number, max?: number) => {
+  if (min === undefined) {
+    min = 0;
+    max = 1;
+  } else if (max === undefined) {
+    max = min;
+    min = 0;
+  }
+  return min + Math.random() * (max - min);
+};
+
+function createCircleVertices({
+  radius = 1,
+  numSubdivisions = 24,
+  innerRadius = 0,
+  startAngle = 0,
+  endAngle = Math.PI * 2,
+} = {}) {
+  // 2 triangles per subdivision, 3 verts per tri, 2 values (xy) each.
+  const numVertices = numSubdivisions * 3 * 2;
+  const vertexData = new Float32Array(numSubdivisions * 2 * 3 * 2);
+
+  let offset = 0;
+  const addVertex = (x: number, y: number) => {
+    vertexData[offset++] = x;
+    vertexData[offset++] = y;
+  };
+
+  // 2 triangles per subdivision
+  //
+  // 0--1 4
+  // | / /|
+  // |/ / |
+  // 2 3--5
+  for (let i = 0; i < numSubdivisions; ++i) {
+    const angle1 =
+      startAngle + ((i + 0) * (endAngle - startAngle)) / numSubdivisions;
+    const angle2 =
+      startAngle + ((i + 1) * (endAngle - startAngle)) / numSubdivisions;
+
+    const c1 = Math.cos(angle1);
+    const s1 = Math.sin(angle1);
+    const c2 = Math.cos(angle2);
+    const s2 = Math.sin(angle2);
+
+    // first triangle
+    addVertex(c1 * radius, s1 * radius);
+    addVertex(c2 * radius, s2 * radius);
+    addVertex(c1 * innerRadius, s1 * innerRadius);
+
+    // second triangle
+    addVertex(c1 * innerRadius, s1 * innerRadius);
+    addVertex(c2 * radius, s2 * radius);
+    addVertex(c2 * innerRadius, s2 * innerRadius);
+  }
+
+  return {
+    vertexData,
+    numVertices,
+  };
+}
 
 export class Renderer {
   device: GPUDevice;
@@ -22,13 +93,14 @@ export class Renderer {
   renderTarget: GPUTexture | undefined;
   presentationFormat: GPUTextureFormat;
   initialized: boolean;
+  bindGroup: GPUBindGroup;
+  storageValues: Float32Array;
+  changingStorageBuffer: GPUBuffer;
+  numVertices: number;
 
   kNumObjects = 100;
   objectInfos: {
     scale: number;
-    uniformBuffer: GPUBuffer;
-    bindGroup: GPUBindGroup;
-    uniformValues: Float32Array;
   }[] = [];
 
   private onFrameHandler: () => void;
@@ -100,75 +172,80 @@ export class Renderer {
       },
     });
 
-    // A random number between [min and max)
-    // With 1 argument it will be [0 to min)
-    // With no arguments it will be [0 to 1)
-    const rand = (min?: number, max?: number) => {
-      if (min === undefined) {
-        min = 0;
-        max = 1;
-      } else if (max === undefined) {
-        max = min;
-        min = 0;
+    const staticStorageBufferSize = staticUnitSize * this.kNumObjects;
+    const changingStorageBufferSize = changingUnitSize * this.kNumObjects;
+
+    // setup a storage buffer with vertex data
+    const { vertexData, numVertices } = createCircleVertices({
+      radius: 0.5,
+      innerRadius: 0.25,
+    });
+    const vertexStorageBuffer = device.createBuffer({
+      label: 'storage buffer vertices',
+      size: vertexData.byteLength,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+    });
+    device.queue.writeBuffer(vertexStorageBuffer, 0, vertexData);
+    this.numVertices = numVertices;
+
+    const staticStorageBuffer = device.createBuffer({
+      label: 'static storage for objects',
+      size: staticStorageBufferSize,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+    });
+
+    const changingStorageBuffer = device.createBuffer({
+      label: 'changing storage for objects',
+      size: changingStorageBufferSize,
+      usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+    });
+
+    {
+      const staticStorageValues = new Float32Array(staticStorageBufferSize / 4);
+      for (let i = 0; i < this.kNumObjects; ++i) {
+        const staticOffset = i * (staticUnitSize / 4);
+
+        // These are only set once so set them now
+        // set the color
+        staticStorageValues.set(
+          [rand(), rand(), rand(), 1],
+          staticOffset + kColorOffset
+        );
+
+        // set the offset
+        staticStorageValues.set(
+          [rand(-0.9, 0.9), rand(-0.9, 0.9)],
+          staticOffset + kOffsetOffset
+        ); // set the offset
+
+        this.objectInfos.push({
+          scale: rand(0.2, 0.5),
+        });
       }
-      return min + Math.random() * (max - min);
-    };
 
-    // create 2 buffers for the uniform values
-    const staticUniformBufferSize =
-      4 * 4 + // color is 4 32bit floats (4bytes each)
-      2 * 4 + // offset is 2 32bit floats (4bytes each)
-      2 * 4; // padding
-    const uniformBufferSize = 2 * 4; // scale is 2 32bit floats (4bytes each)
-
-    for (let i = 0; i < this.kNumObjects; ++i) {
-      const staticUniformBuffer = device.createBuffer({
-        label: `static uniforms for obj: ${i}`,
-        size: staticUniformBufferSize,
-        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-      });
-
-      // These are only set once so set them now
-      {
-        const uniformValues = new Float32Array(staticUniformBufferSize / 4);
-        uniformValues.set([rand(), rand(), rand(), 1], kColorOffset); // set the color
-        uniformValues.set([rand(-0.9, 0.9), rand(-0.9, 0.9)], kOffsetOffset); // set the offset
-
-        // copy these values to the GPU
-        device.queue.writeBuffer(staticUniformBuffer, 0, uniformValues);
-      }
-
-      // create a typedarray to hold the values for the uniforms in JavaScript
-      const uniformValues = new Float32Array(uniformBufferSize / 4);
-      const uniformBuffer = device.createBuffer({
-        label: `changing uniforms for obj: ${i}`,
-        size: uniformBufferSize,
-        usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-      });
-
-      const bindGroup = device.createBindGroup({
-        label: `bind group for obj: ${i}`,
-        layout: pipeline.getBindGroupLayout(0),
-        entries: [
-          { binding: 0, resource: { buffer: staticUniformBuffer } },
-          { binding: 1, resource: { buffer: uniformBuffer } },
-        ],
-      });
-
-      this.objectInfos.push({
-        scale: rand(0.2, 0.5),
-        uniformBuffer,
-        uniformValues,
-        bindGroup,
-      });
+      device.queue.writeBuffer(staticStorageBuffer, 0, staticStorageValues);
     }
 
-    requestAnimationFrame(this.onFrameHandler);
+    // a typed array we can use to update the changingStorageBuffer
+    this.storageValues = new Float32Array(changingStorageBufferSize / 4);
 
+    this.bindGroup = device.createBindGroup({
+      label: 'bind group for objects',
+      layout: pipeline.getBindGroupLayout(0),
+      entries: [
+        { binding: 0, resource: { buffer: staticStorageBuffer } },
+        { binding: 1, resource: { buffer: changingStorageBuffer } },
+        { binding: 2, resource: { buffer: vertexStorageBuffer } },
+      ],
+    });
+
+    this.changingStorageBuffer = changingStorageBuffer;
     this.initialized = true;
     this.context = context;
     this.device = device;
     this.pipeline = pipeline;
+
+    requestAnimationFrame(this.onFrameHandler);
   }
 
   onFrame() {
@@ -182,9 +259,8 @@ export class Renderer {
     this.disposed = true;
   }
 
-  async render() {
+  prepareRender() {
     const device = this.device;
-    const context = this.context;
     const canvas = this.pane.canvas()!;
     const currentWidth = canvas.width;
     const currentHeight = canvas.height;
@@ -220,9 +296,17 @@ export class Renderer {
       this.prevHeight = currentHeight;
       this.prevWidth = currentWidth;
     }
+  }
+
+  async render() {
+    this.prepareRender();
+    const device = this.device;
+    const context = this.context;
+    const canvas = this.pane.canvas()!;
+    let renderTargetView = this.renderTargetView;
 
     if (renderTargetView) {
-      const commandEncoder = device.createCommandEncoder();
+      const encoder = device.createCommandEncoder();
       const renderPassDescriptor: GPURenderPassDescriptor = {
         colorAttachments: [
           {
@@ -235,24 +319,31 @@ export class Renderer {
         ],
       };
 
-      const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
-      passEncoder.setPipeline(this.pipeline);
+      const pass = encoder.beginRenderPass(renderPassDescriptor);
+      pass.setPipeline(this.pipeline);
 
       // Set the uniform values in our JavaScript side Float32Array
       const aspect = canvas.width / canvas.height;
 
-      for (const { scale, bindGroup, uniformBuffer, uniformValues } of this
-        .objectInfos) {
-        uniformValues.set([scale / aspect, scale], kScaleOffset); // set the scale
-        device.queue.writeBuffer(uniformBuffer, 0, uniformValues);
-        passEncoder.setBindGroup(0, bindGroup);
-        passEncoder.draw(3); // call our vertex shader 3 times
-      }
+      // set the scales for each object
+      this.objectInfos.forEach(({ scale }, ndx) => {
+        const offset = ndx * (changingUnitSize / 4);
+        this.storageValues.set([scale / aspect, scale], offset + kScaleOffset); // set the scale
+      });
+      // upload all scales at once
+      device.queue.writeBuffer(
+        this.changingStorageBuffer,
+        0,
+        this.storageValues
+      );
 
-      passEncoder.draw(3);
-      passEncoder.end();
+      pass.setBindGroup(0, this.bindGroup);
+      pass.draw(this.numVertices, this.kNumObjects); // ca
 
-      device.queue.submit([commandEncoder.finish()]);
+      pass.end();
+
+      const commandBuffer = encoder.finish();
+      device.queue.submit([commandBuffer]);
     }
   }
 }
--- packages/rewild-renderer/lib/shaders/simple.wgsl
+++ packages/rewild-renderer/lib/shaders/simple.wgsl
@@ -14,24 +14,36 @@ struct OtherStruct {
   scale: vec2f,
 };
 
-@group(0) @binding(0) var<uniform> ourStruct: OurStruct;
-@group(0) @binding(1) var<uniform> otherStruct: OtherStruct;
+struct Vertex {
+  position: vec2f,
+};
+
+struct VSOutput {
+  @builtin(position) position: vec4f,
+  @location(0) color: vec4f,
+}
+
+@group(0) @binding(0) var<storage, read> ourStructs: array<OurStruct>;
+@group(0) @binding(1) var<storage, read> otherStructs: array<OtherStruct>;
+@group(0) @binding(2) var<storage, read> pos: array<Vertex>;
 
 @vertex
 fn vs(
-  @builtin(vertex_index) vertexIndex : u32
-) -> @builtin(position) vec4f {
-  var pos = array<vec2f, 3>(
-    vec2f(0.0, 0.5),
-    vec2f(-0.5, -0.5),
-    vec2f(0.5, -0.5)
-  );
+  @builtin(vertex_index) vertexIndex : u32,
+  @builtin(instance_index) instanceIndex: u32
+) -> VSOutput {
 
-  return vec4f(
-    pos[vertexIndex] * otherStruct.scale + ourStruct.offset, 0.0, 1.0);
+  let otherStruct = otherStructs[instanceIndex];
+  let ourStruct = ourStructs[instanceIndex];
+
+  var vsOut: VSOutput;
+  vsOut.position = vec4f(
+      pos[vertexIndex].position * otherStruct.scale + ourStruct.offset, 0.0, 1.0);
+
+  vsOut.color = ourStruct.color;
+  return vsOut;
 }
-  
-@fragment
-fn fs() -> @location(0) vec4f {
-  return ourStruct.color;
+
+@fragment fn fs(vsOut: VSOutput) -> @location(0) vec4f {
+  return vsOut.color;
 }
