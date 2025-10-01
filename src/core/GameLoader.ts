d372388fe46604955b299137396d3a605b90d879
Mathew Henson <mathewkhenson@gmail.com>
2025-10-01 22:23:12 +0100

    Extracted template loader so it can be re-used in editor

--- src/core/GameLoader.ts
+++ src/core/GameLoader.ts
@@ -3,16 +3,15 @@ import { getProjects } from '../api/projects';
 import { Container, StateMachine } from 'rewild-routing';
 import { InGameLevel } from './routing/InGameLevel';
 import { Asset3D } from './routing/Asset3D';
-import { Mesh, Renderer } from 'rewild-renderer';
+import { Renderer } from 'rewild-renderer';
 import { Player } from './routing/Player';
-import { ITemplateItems, Vector3 } from 'models';
+import { TemplateLoader } from './TemplateLoader';
 
 /** Loads game files and assets and sends the created objects to wasm */
 
 export async function loadInitialLevels(player: Player, renderer: Renderer) {
-  const templateLibrary = (await fetch('/templates/template-library.json').then(
-    (res) => res.json()
-  )) as ITemplateItems;
+  const templateLoader = new TemplateLoader();
+  await templateLoader.load();
 
   const projects = await getProjects(true);
   const startupLevels = await Promise.all(
@@ -39,45 +38,12 @@ export async function loadInitialLevels(player: Player, renderer: Renderer) {
       levelRouter.addChild(containerRouter);
 
       for (const actor of container.actors) {
-        let object: Asset3D | null = null;
-        const preset = actor.properties.find((p) => p.type === 'templateId');
+        let object = (await templateLoader.createResource(
+          actor,
+          renderer
+        )) as Asset3D;
 
-        const template = preset
-          ? templateLibrary.assets.find((asset) => asset.name === preset.value)
-          : null;
-
-        if (template?.resource.geometryId && template?.resource.materialId) {
-          const mesh = new Mesh(
-            renderer.geometryManager.get(template?.resource.geometryId),
-            renderer.materialManager.get(template?.resource.materialId)
-          );
-          mesh.transform.name = actor.name || 'Mesh';
-          object = new Asset3D(mesh.transform);
-          const initialPos =
-            (actor.properties.find((p) => p.type === 'position')
-              ?.value as Vector3) || null;
-          if (initialPos)
-            object.initialPosition.set(
-              initialPos[0],
-              initialPos[1],
-              initialPos[2]
-            );
-        }
-        // else if (
-        //   actorLoaderPreset === 'directional-light' ||
-        //   actorLoaderPreset === 'ambient-light'
-        // ) {
-        //   if (actorLoaderPreset === 'directional-light') {
-        //     object = new DirectionalLight(actor.name);
-        //   } else if (actorLoaderPreset === 'ambient-light') {
-        //     object = new AmbientLight(actor.name);
-        //   }
-        // }
-
-        // Add the object to the container if it exists
-        if (object) {
-          levelRouter.addAsset(object);
-        }
+        levelRouter.addAsset(object);
       }
     }
   }
