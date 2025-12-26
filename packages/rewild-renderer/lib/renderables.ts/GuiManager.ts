8dda64768c7965a18f09146638e8190f5055ad65
Mathew Henson <mathewkhenson@gmail.com>
2025-12-26 23:20:05 +0000

    Added fall damage check to the player

--- packages/rewild-renderer/lib/renderables.ts/GuiManager.ts
+++ packages/rewild-renderer/lib/renderables.ts/GuiManager.ts
@@ -1,6 +1,5 @@
 import { UIElement } from '../core/UIElement';
 import { IMaterialPass } from '../materials/IMaterialPass';
-import { UIElementHealthPass } from '../materials/UIElementHealthPass';
 import { Renderer } from '../Renderer';
 
 export class GuiManager {
@@ -22,32 +21,18 @@ export class GuiManager {
 
     canvas.removeEventListener('click', this.onClickDelegate);
     canvas.addEventListener('click', this.onClickDelegate);
-
-    const uiHealthMaterial = renderer.materialManager.get(
-      'ui-health-material'
-    ) as UIElementHealthPass;
-
-    const healthBar = this.createElement(uiHealthMaterial);
-    renderer.ui.addChild(healthBar.transform);
-    healthBar.borderRadius = 20;
-    healthBar.width = 0.4;
-    healthBar.height = 0.05;
-    healthBar.x = 0.3;
-    healthBar.y = 0.92;
-    healthBar.percentageBasedCalculation = true;
-
     return this;
   }
 
   onClick(e: MouseEvent): void {
-    const uiMaterial = this.renderer.materialManager.get('ui-material');
-    const newElement = this.createElement(uiMaterial);
-    this.renderer.ui.addChild(newElement.transform);
-    newElement.x = Math.random() * 800;
-    newElement.y = Math.random() * 800;
-    newElement.width = Math.random() * 500;
-    newElement.height = Math.random() * 300;
-    newElement.borderRadius = 10;
+    // const uiMaterial = this.renderer.materialManager.get('ui-material');
+    // const newElement = this.createElement(uiMaterial);
+    // this.renderer.ui.addChild(newElement.transform);
+    // newElement.x = Math.random() * 800;
+    // newElement.y = Math.random() * 800;
+    // newElement.width = Math.random() * 500;
+    // newElement.height = Math.random() * 300;
+    // newElement.borderRadius = 10;
   }
 
   createElement(material: IMaterialPass): UIElement {
