ecd0fc523ada441d7e85590342a4caddf84d66b7
Mathew Henson <mathewkhenson@gmail.com>
2026-03-26 22:06:25 +0000

    feat: introduce overlay rendering support in Renderer and enhance Gizmo with plane meshes and hover interaction

--- packages/rewild-renderer/lib/index.ts
+++ packages/rewild-renderer/lib/index.ts
@@ -1,5 +1,6 @@
 export * from './Renderer';
 export * from './core/Transform';
+export * from './core/RenderLayer';
 export * from './core/Camera';
 export * from './core/PerspectiveCamera';
 export * from './core/OrthographicCamera';
